94ac3e04d86459a81fc04a53a491f07a1a1b7920
mucl <murillocasemiro@gmail.com>
2023-05-05 15:24:01 -0300

    ta chegando la

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 
-import ColorR from './components/ColorR';
-import ColorG from './components/ColorG';
+
 import BuscaPessoa from './components/busca-pessoa';
 
 function App() {
@@ -11,20 +10,20 @@ function App() {
     <>
       <header>
         <div className="btn-nav">
-          <Link to='/pessoa1'>ColorR</Link>
+          <Link to='/pessoa1'>Busca jogador 1</Link>
         </div>
         <div className="btn-nav">
-          <Link to='/pessoa2'>ColorG</Link>
+          <Link to='/pessoa2'>Busca jogador 2</Link>
         </div>
         <div className="btn-nav">
-          <Link to='./pessoa3'>BuscaPessoa</Link>
+          <Link to='./pessoa3'>Busca jogador 3</Link>
         </div>
       </header>
       <main>
         <Routes>
-          <Route path='/pessoa1' element={<ColorR/>}/>
-          <Route path='/pessoa2' element={<ColorG/>}/>
-          <Route path='/pessoa3' element={<BuscaPessoa id={1} bgc='blue'/>}/>
+          <Route path='/pessoa1' element={<BuscaPessoa id={1} bgc='blue'/>}/>
+          <Route path='/pessoa2' element={<BuscaPessoa id={2} bgc='blue'/>}/>
+          <Route path='/pessoa3' element={<BuscaPessoa id={3} bgc='blue'/>}/>
         </Routes>
       </main>
     </>
--- front-end/src/components/busca-pessoa.js
+++ front-end/src/components/busca-pessoa.js
@@ -1,39 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function BuscaPessoa(props) {
-    const { id, bgc } = props;
-    const [pessoa, setPessoa] = useState([]);
+    let { id, bgc } = props;
+    let [pessoa, setPessoa] = useState([]);
 
-    function buscaId(event) {
-        fetch(`http://localhost:5000/pessoas/${id}`)
+    useEffect(() => {
+        buscaId()
+    }, []);
+
+
+    function buscaId() {
+
+        let newId = id 
+
+        fetch(`http://localhost:5000/pessoas/${newId}`)
             .then((response) => response.json())
             .then((data) => {
                 setPessoa(data);
-                outraBanana();
-
             })
             .catch((error) => {
                 console.error(error);
             });
     }
 
-    function outraBanana() {
-        return (
-            <div>
-                <button onClick={buscaId}>Buscar</button>
-                {pessoa && (
-                    <div key={pessoa.id} className="res-container" style={{ backgroundColor: bgc }}>
-                        <h1>{pessoa.nome}</h1>
-                        <h3>{pessoa.nickname}</h3>
-                        <div className="back-to-start-page">
-                            <Link to="../App.js">esconder</Link>
-                        </div>
+    return (
+        <div>
+            {pessoa && (
+                <div className="res-container" style={{ backgroundColor: bgc }}>
+                    {console.log(pessoa)}
+                    <h1>{pessoa.nome}</h1>
+                    <h3>{pessoa.nickname}</h3>
+                    <div className="back-to-start-page">
+                        <Link to="../App.js">limpar cache</Link>
                     </div>
-                )}
-            </div>
-        )
-    }
-
-    return outraBanana();
+                </div>
+            )}
+        </div>
+    );
 }
